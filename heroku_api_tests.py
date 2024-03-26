8a581cf2fe39b8c4f71ea474786713c442e68de9
mikethwolff <3strom@web.de>
2024-03-26 20:10:14 +0100

    Amended heroku test

---
+++ heroku_api_tests.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+if __name__ == "__main__":
+    
+    r = requests.get("https://census-salaries.herokuapp.com/")
+    print(r.text)
+
+    # Test data
+    d = {     
+        'age': 31,
+        'workclass': 'Private',
+        'fnlgt': 45781,
+        'education': 'Masters',
+        'education_num': 14,
+        'marital_status': 'Never-married',
+        'occupation': 'Prof-specialty',
+        'relationship': 'Not-in-family',
+        'race': 'White',
+        'sex': 'Female',
+        'capital_gain': 14084,
+        'capital_loss': 0,
+        'hours_per_week': 50,
+        'native_country': 'United-States'
+        }
+
+    response = requests.post("https://census-salaries.herokuapp.com/predict", json=d)
+    if response.status_code == 200:
+        print("POST request successful.")
+        print(response.text)
+    else:
+        print("POST request failed with status code:", response.status_code)
