a314edf068dd086f9749beae6f5cd68e92fc7570
Michael Wolff <3strom@web.de>
2024-03-27 21:19:45 +0100

    Add files via upload

--- heroku_api_tests.py
+++ heroku_api_tests.py
@@ -13,20 +13,20 @@ else:
 
 # Test data
 d = {     
-    'age': 31,
-    'workclass': 'Private',
-    'fnlgt': 45781,
-    'education': 'Masters',
-    'education_num': 14,
-    'marital_status': 'Never-married',
-    'occupation': 'Prof-specialty',
-    'relationship': 'Not-in-family',
-    'race': 'White',
-    'sex': 'Female',
-    'capital_gain': 14084,
-    'capital_loss': 0,
-    'hours_per_week': 50,
-    'native_country': 'United-States'
+    "age": 31,
+    "workclass": "Private",
+    "fnlgt": 45781,
+    "education": "Masters",
+    "education_num": 14,
+    "marital_status": "Never-married",
+    "occupation": "Prof-specialty",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Female",
+    "capital_gain": 14084,
+    "capital_loss": 0,
+    "hours_per_week": 50,
+    "native_country": "United-States"
     }
 
 response = requests.post("https://census-salaries-d3e2956470bf.herokuapp.com/predict", json=d)
