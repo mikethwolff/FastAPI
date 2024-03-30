ecba0ac0606844fd835f1b489e73e191941ffd39
Michael Wolff <3strom@web.de>
2024-03-30 14:01:12 +0100

    Add files via upload

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,54 +1,53 @@
 from fastapi.testclient import TestClient
-
-if __package__ is None or __package__ == "":
-    from main import app
-else:
-    from ..main import app
+from main import app
 
 client = TestClient(app)
 
-def test_welcome_msg():
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert resp.json() == "<html><body><p>CensusAPI</p><p>Documentation can be found <a href='/docs'>here</a>.</p></body></html>"
+def testWelcomeMsg():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.text == "<html><body><p>CensusAPI</p><p>Documentation can be found <a href='/docs'>here</a>.</p></body></html>"
 
+def testSalaryUnder50k():
+    data = {
+            'age': 39,
+            'workclass': 'State-gov',
+            'fnlgt': 77516,
+            'education': 'Bachelors',
+            'education_num': 13,
+            'marital_status': 'Never-married',
+            'occupation': 'Adm-clerical',
+            'relationship': 'Not-in-family',
+            'race': 'White',
+            'sex': 'Male',
+            'capital_gain': 2174,
+            'capital_loss': 0,
+            'hours_per_week': 40,
+            'native_country': 'United-States'
+            }
+    
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200
+    assert response.text == '[ RESULT: The predicted income is: <=50K ]'
 
-def test_lower_50k():
-    test = {"age": 39,
-              "workclass": "State-gov",
-              "fnlgt": 77516,
-              "education": "Bachelors",
-              "education-num": 13,
-              "marital-status": "Never-married",
-              "occupation": "Adm-clerical",
-              "relationship": "Not-in-family",
-              "race": "White",
-              "sex": "Male",
-              "capital-gain": 2174,
-              "capital-loss": 0,
-              "hours-per-week": 40,
-              "native-country": "United-States"}
-    resp = client.post("/predict", json=test)
-    assert resp.status_code == 200
-    assert resp.json() == {"predicted_salary": "<=50K"}
-
-
-def test_greater_50k():
-    test = {"age": 31,
-              "workclass": "Private",
-              "fnlgt": 45781,
-              "education": "Masters",
-              "education-num": 14,
-              "marital-status": "Never-married",
-              "occupation": "Prof-specialty",
-              "relationship": "Not-in-family",
-              "race": "White",
-              "sex": "Female",
-              "capital-gain": 14084,
-              "capital-loss": 0,
-              "hours-per-week": 50,
-              "native-country": "United-States"}
-
-    resp = client.post("/predict", json=test)
-    assert resp.status_code == 200
-    assert resp.json() == {"predicted_salary": ">50K"}
+def testSalaryOver50k():
+    data = {     
+            'age': 31,
+            'workclass': 'Private',
+            'fnlgt': 45781,
+            'education': 'Masters',
+            'education_num': 14,
+            'marital_status': 'Never-married',
+            'occupation': 'Prof-specialty',
+            'relationship': 'Not-in-family',
+            'race': 'White',
+            'sex': 'Female',
+            'capital_gain': 14084,
+            'capital_loss': 0,
+            'hours_per_week': 50,
+            'native_country': 'United-States'
+            }
+    
+    response = client.post("/predict", json=data)
+    assert response.status_code == 200
+    assert response.text == '[ RESULT: The predicted income is: >50K ]'
---
+++ tests/test_cleandata.py
@@ -0,0 +1,18 @@
+import pytest
+import pandas as pd
+from ml.clean_data import basic_cleaning
+
+@pytest.fixture(scope="session")
+def loadData():
+    data = pd.read_csv("tests/testdata/census_test.csv")
+    return data
+
+def testLoadData(loadData):
+    assert loadData.shape == (100, 15)
+
+def testCleaning(loadData):
+    df, cat_cols, num_cols = basic_cleaning(loadData, "tests/testdata/census_cleaned.csv", "salary", test=True)
+    assert df.shape == (100, 15)
+    assert df.isna().sum().sum() == 0
+    assert len(cat_cols) == 9
+    assert len(num_cols) == 6
