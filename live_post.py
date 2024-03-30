b2bca0da43ef387478c055663c265dfca8778bdd
Michael Wolff <3strom@web.de>
2024-03-30 17:52:15 +0100

    Add files via upload

--- live_post.py
+++ live_post.py
@@ -1,6 +1,4 @@
 import requests
-import json
-from fastapi.responses import HTMLResponse
 
 response = requests.get("https://census-salaries-d3e2956470bf.herokuapp.com/")
 # Check the response status code
